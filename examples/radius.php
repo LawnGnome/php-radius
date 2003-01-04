f48417dedffe1fefcc1679ec0db4ae0e48cea354
mj <mj@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-01-04 11:56:31 +0000

    * Update copyright year to 2003.
    git-svn-id: https://svn.php.net/repository/pecl/radius/trunk@110401 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/radius.php
+++ examples/radius.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (c) 2002, Michael Bretterklieber <michael@bretterklieber.com>
+Copyright (c) 2002-2003, Michael Bretterklieber <michael@bretterklieber.com>
 All rights reserved.
  
 Redistribution and use in source and binary forms, with or without 
--- radlib_vs.h
+++ radlib_vs.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2002 Brian Somers <brian@Awfulhak.org>
+ * Copyright (c) 2002-2003 Brian Somers <brian@Awfulhak.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
