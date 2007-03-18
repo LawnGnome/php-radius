302a778fbd38a318a0a07ebd447d399e5f4f67e6
cvs2svn <cvs2svn@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-03-18 21:50:06 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_2_5'.
    git-svn-id: https://svn.php.net/repository/pecl/radius/tags/RELEASE_1_2_5@232106 c90b9560-bf6c-de11-be94-00142212c4b1

--- extract_const.sh
+++
@@ -1,2 +0,0 @@
-grep "#define" radlib.h | grep -v "_RADLIB_H_" | sed -E 's/#define. *RAD_([A-Z_0-9]+)[^0-9]+([0-9]+)/REGISTER_LONG_CONSTANT("RADIUS_\1", \2, CONST_PERSISTENT);/' > radius_init_const.h
-grep "#define" radlib_vs.h | grep -v "_RADLIB_VS_H_" | sed -E 's/#define. *RAD_([A-Z_0-9]+)[^0-9]+([0-9]+)/REGISTER_LONG_CONSTANT("RADIUS_\1", \2, CONST_PERSISTENT);/'  >> radius_init_const.h
