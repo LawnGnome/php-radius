d6172b16187596eec99e8f5a910002b6ac09bd45
Adam Harvey <aharvey@php.net>
2013-06-18 13:16:56 -0700

    Actually generate an error when there aren't any servers.

--- radlib.c
+++ radlib.c
@@ -980,8 +980,10 @@ rad_request_authenticator(struct rad_handle *h, char *buf, size_t len)
 const char *
 rad_server_secret(struct rad_handle *h)
 {
-	if (h->srv >= h->num_servers)
+	if (h->srv >= h->num_servers) {
+		generr(h, "No RADIUS servers specified");
 		return NULL;
+	}
 
 	return (h->servers[h->srv].secret);
 }
