03337b25df6002a59f2baf0b1f1b689226e144d6
Adam Harvey <aharvey@php.net>
2013-06-20 10:51:47 -0700

    Drop the CVS revision from phpinfo(), since it's useless post-CVS.

--- radius.c
+++ radius.c
@@ -142,7 +142,6 @@ PHP_MINFO_FUNCTION(radius)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "radius support", "enabled");
 	php_info_print_table_row(2, "version", PHP_RADIUS_VERSION);
-	php_info_print_table_row(2, "CVS revision", "$Revision$");
 	php_info_print_table_end();
 }
 /* }}} */
