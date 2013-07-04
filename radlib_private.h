794dc5e3e8de10c9b1e9b73384e0ad32bc30f604
Bruno Prémont <bruno.premont@restena.lu>
2013-07-04 09:50:37 +0200

    Add support for IPv6
    Next to IPv6 suport socket handling is reworked to better operate in
    presence of multiple servers (IPv4 or IPv6) where either some
    destination networks are unreachable or otherwise fail on sendmsg
    (e.g. due to local firewall rules).
    
    It makes use of two sockets (one for IPv4 communication, one for IPv6
    communication) that are created as needed (e.g. no IPv6 socket created
    if there is no IPv6 server address).
    
    When adding servers by host names, multiple entries are added, one for
    each IP address found for the hostname (be it IPv4 or IPv6).
    
    NOTE: Windows compatibility is unknown.
    
    See also PHP bug #59619

--- radlib_private.h
+++ radlib_private.h
@@ -66,7 +66,11 @@
 #define POS_ATTRS	20		/* Start of attributes */
 
 struct rad_server {
-	struct sockaddr_in addr;	/* Address of server */
+	union {
+		struct sockaddr_in addr4;
+		struct sockaddr_in6 addr6;
+		struct sockaddr addr;
+	} addr;				/* Address of server */
 	char		*secret;	/* Shared secret */
 	int		 timeout;	/* Timeout in seconds */
 	int		 max_tries;	/* Number of tries before giving up */
@@ -74,7 +78,8 @@ struct rad_server {
 };
 
 struct rad_handle {
-	int		 fd;		/* Socket file descriptor */
+	int		 fd4;		/* Socket file descriptor */
+	int		 fd6;		/* Socket file descriptor */
 	struct rad_server servers[MAXSERVERS];	/* Servers to contact */
 	int		 num_servers;	/* Number of valid server entries */
 	int		 ident;		/* Current identifier value */
